5fe1a7250fc7406bb40a157831cba76a294b69ba
Kent Javier <146943273+KentJavier@users.noreply.github.com>
2023-11-21 21:18:54 +0800

    4th commit
    Update: I created a new Visual Studio and SQL Database for my system and the last error problem has been fixed. Will now try to create a login and registration for my system.

---
+++ Library System/Library System/Login.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Library_System
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPasswordLogin = new System.Windows.Forms.TextBox();
+            this.lblPasswordLogin = new System.Windows.Forms.Label();
+            this.txtUsernameLogin = new System.Windows.Forms.TextBox();
+            this.lblUsernameLogin = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.Color.White;
+            this.pnlLogin.Controls.Add(this.pictureBox2);
+            this.pnlLogin.Controls.Add(this.pictureBox1);
+            this.pnlLogin.Controls.Add(this.btnRegister);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.txtPasswordLogin);
+            this.pnlLogin.Controls.Add(this.lblPasswordLogin);
+            this.pnlLogin.Controls.Add(this.txtUsernameLogin);
+            this.pnlLogin.Controls.Add(this.lblUsernameLogin);
+            this.pnlLogin.Location = new System.Drawing.Point(46, 25);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(312, 307);
+            this.pnlLogin.TabIndex = 2;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRegister.Font = new System.Drawing.Font("Lucida Sans Unicode", 7.8F);
+            this.btnRegister.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.btnRegister.Location = new System.Drawing.Point(185, 244);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(97, 38);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "REGISTER";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLogin.Font = new System.Drawing.Font("Lucida Sans Unicode", 7.8F);
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.btnLogin.Location = new System.Drawing.Point(63, 244);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(86, 38);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtPasswordLogin
+            // 
+            this.txtPasswordLogin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPasswordLogin.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtPasswordLogin.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtPasswordLogin.Location = new System.Drawing.Point(62, 176);
+            this.txtPasswordLogin.Name = "txtPasswordLogin";
+            this.txtPasswordLogin.Size = new System.Drawing.Size(220, 33);
+            this.txtPasswordLogin.TabIndex = 2;
+            // 
+            // lblPasswordLogin
+            // 
+            this.lblPasswordLogin.AutoSize = true;
+            this.lblPasswordLogin.Font = new System.Drawing.Font("Lucida Sans Unicode", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.lblPasswordLogin.Location = new System.Drawing.Point(101, 133);
+            this.lblPasswordLogin.Name = "lblPasswordLogin";
+            this.lblPasswordLogin.Size = new System.Drawing.Size(128, 31);
+            this.lblPasswordLogin.TabIndex = 3;
+            this.lblPasswordLogin.Text = "Password";
+            // 
+            // txtUsernameLogin
+            // 
+            this.txtUsernameLogin.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtUsernameLogin.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtUsernameLogin.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsernameLogin.Location = new System.Drawing.Point(62, 83);
+            this.txtUsernameLogin.Name = "txtUsernameLogin";
+            this.txtUsernameLogin.Size = new System.Drawing.Size(220, 33);
+            this.txtUsernameLogin.TabIndex = 1;
+            // 
+            // lblUsernameLogin
+            // 
+            this.lblUsernameLogin.AutoSize = true;
+            this.lblUsernameLogin.BackColor = System.Drawing.Color.White;
+            this.lblUsernameLogin.Font = new System.Drawing.Font("Lucida Sans Unicode", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsernameLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.lblUsernameLogin.Location = new System.Drawing.Point(94, 40);
+            this.lblUsernameLogin.Name = "lblUsernameLogin";
+            this.lblUsernameLogin.Size = new System.Drawing.Size(135, 31);
+            this.lblUsernameLogin.TabIndex = 1;
+            this.lblUsernameLogin.Text = "Username";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(19, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(19, 176);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(405, 361);
+            this.Controls.Add(this.pnlLogin);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LibraLearn Login";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPasswordLogin;
+        private System.Windows.Forms.Label lblPasswordLogin;
+        private System.Windows.Forms.TextBox txtUsernameLogin;
+        private System.Windows.Forms.Label lblUsernameLogin;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Library System/Library System/Registration.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Library_System
+{
+    partial class btnRegisterAcc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btnRegisterAcc));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRegisterAccount = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.dtpRegistrationDate = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.dtpRegistrationDate);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.btnRegisterAccount);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtLastName);
+            this.panel1.Controls.Add(this.txtFirstName);
+            this.panel1.Location = new System.Drawing.Point(468, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 447);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRegisterAccount
+            // 
+            this.btnRegisterAccount.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRegisterAccount.Font = new System.Drawing.Font("Lucida Sans Unicode", 7.8F);
+            this.btnRegisterAccount.Location = new System.Drawing.Point(82, 392);
+            this.btnRegisterAccount.Name = "btnRegisterAccount";
+            this.btnRegisterAccount.Size = new System.Drawing.Size(134, 33);
+            this.btnRegisterAccount.TabIndex = 4;
+            this.btnRegisterAccount.Text = "Register account";
+            this.btnRegisterAccount.UseVisualStyleBackColor = false;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtEmail.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtEmail.ForeColor = System.Drawing.Color.DimGray;
+            this.txtEmail.Location = new System.Drawing.Point(43, 155);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(209, 33);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Text = "  Enter email here...";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtLastName.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtLastName.ForeColor = System.Drawing.Color.DimGray;
+            this.txtLastName.Location = new System.Drawing.Point(43, 97);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(209, 33);
+            this.txtLastName.TabIndex = 2;
+            this.txtLastName.Text = "Enter last name here...";
+            this.txtLastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtFirstName.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtFirstName.ForeColor = System.Drawing.Color.DimGray;
+            this.txtFirstName.Location = new System.Drawing.Point(43, 38);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(209, 33);
+            this.txtFirstName.TabIndex = 1;
+            this.txtFirstName.Text = "Enter first name here..";
+            this.txtFirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(25, 22);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(422, 447);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 4;
+            this.pbLogo.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPassword.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.txtPassword.Location = new System.Drawing.Point(43, 270);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(209, 33);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "Enter password here...";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtUsername.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.txtUsername.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUsername.Location = new System.Drawing.Point(43, 211);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(209, 33);
+            this.txtUsername.TabIndex = 5;
+            this.txtUsername.Text = "Enter username here..";
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dtpRegistrationDate
+            // 
+            this.dtpRegistrationDate.CalendarTitleForeColor = System.Drawing.Color.Black;
+            this.dtpRegistrationDate.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F);
+            this.dtpRegistrationDate.Location = new System.Drawing.Point(43, 328);
+            this.dtpRegistrationDate.Name = "dtpRegistrationDate";
+            this.dtpRegistrationDate.Size = new System.Drawing.Size(209, 33);
+            this.dtpRegistrationDate.TabIndex = 7;
+            // 
+            // btnRegisterAcc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(796, 495);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "btnRegisterAcc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Button btnRegisterAccount;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.DateTimePicker dtpRegistrationDate;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
